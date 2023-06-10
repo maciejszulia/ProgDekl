ad2aaabfc06ce656856b230b898e387a0de17031
maciejszulia <maciej.szulia@gmail.com>
2023-06-10 21:40:33 +0200

    + lab01, lab02

---
+++ lab01/klocki1_maciejszulia.pl
@@ -0,0 +1,6 @@
+%PROGRAM: klocki_1
+%Maciej Szulia 154733
+%gr3 lab1
+na(c,a).
+na(c,b).
+na(d,c).
---
+++ lab02/klocki_3.pl
@@ -0,0 +1,7 @@
+na(d,c).
+na(c,a).
+na(c,b).
+na(a,e).
+na(b,g).
+nad(X,Y):-na(X,Y).
+nad(X,Y):-na(X,Z),nad(Z,Y).
